aab23f3eafdf524b1ab778278f05411978915877
ardyun14 <edward.d.yun@vanderbilt.edu>
2014-11-25 20:57:41 -0600

    Initial Commit

---
+++ iPub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPub
+//
+//  Created by Edward Yun on 11/25/14.
+//  Copyright (c) 2014 Edward Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPub
+//
+//  Created by Edward Yun on 11/25/14.
+//  Copyright (c) 2014 Edward Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
